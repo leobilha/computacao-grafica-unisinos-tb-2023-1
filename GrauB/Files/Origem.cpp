e1093de64f0faefc5f6ce2c17675e871b97006eb
Leonardo Terragno <leonardo.t@atppoa.com.br>
2023-06-22 16:56:17 -0300

    comments pra ajudar os guri (ignore o no clean code, please!)

--- GrauB/Files/Origem.cpp
+++ GrauB/Files/Origem.cpp
@@ -27,9 +27,9 @@ const unsigned int SCR_HEIGHT = 600;
 float camSpeed = 0.01f;
 float dirSpeed = 0.01f;
 
-glm::vec3	cameraPos, 
-			cameraFront, 
-			cameraUp;
+glm::vec3	cameraPos,
+cameraFront,
+cameraUp;
 
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
@@ -79,7 +79,7 @@ int main()
 	glm::mat4 ModelMatrix(1.f);
 
 	cameraPos = glm::vec3(0.0, 0.0, 3.0);
-	cameraFront = glm::vec3(0.0, 0.0, -1.0); 
+	cameraFront = glm::vec3(0.0, 0.0, -1.0);
 	cameraUp = glm::vec3(0.0, 1.0, 0.0);
 
 	ProjectionMatrix = glm::perspective(glm::radians(fov), static_cast<float>(framebufferWidth) / framebufferHeight, nearPlane, farPlane);
@@ -111,59 +111,60 @@ int main()
 
 	while (!glfwWindowShouldClose(window))
 	{
+		processarEntradaTecladoObjeto(window); // Processa o input do teclado para o objeto
+		processarEntradaTecladoObjeto(window, listaPlanetas[planetSelecionado - 1]->position, listaPlanetas[planetSelecionado - 1]->rotation, listaPlanetas[planetSelecionado - 1]->scale, planetSelecionado); // Processa o input do teclado para o planeta selecionado
 
-		processarEntradaTecladoObjeto(window);
-		processarEntradaTecladoObjeto(window, listaPlanetas[planetSelecionado - 1]->position, listaPlanetas[planetSelecionado - 1]->rotation, listaPlanetas[planetSelecionado - 1]->scale, planetSelecionado);
+		int mouseState = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT); // Obtém o estado do botão esquerdo do mouse
 
-		int mouseState = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Define a cor de limpeza do framebuffer
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Limpa o framebuffer
 
-		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glUseProgram(shaderProgram); // Usa o programa de shader
 
-		glUseProgram(shaderProgram);
+		glm::mat4 view = glm::mat4(1); // Matriz de visualização
+		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp); // Calcula a matriz de visualização
+		glUniformMatrix4fv(viewPosLoc, 1, FALSE, glm::value_ptr(view)); // Define a matriz de visualização no shader
+		glUniform3f(viewPosLoc, cameraPos.x, cameraPos.y, cameraPos.z); // Define a posição da câmera no shader
 
-		glm::mat4 view = glm::mat4(1);
-		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-		glUniformMatrix4fv(viewPosLoc, 1, FALSE, glm::value_ptr(view));
-		glUniform3f(viewPosLoc, cameraPos.x, cameraPos.y, cameraPos.z);
+		glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight); // Obtém o tamanho do framebuffer
 
-		glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
-
-		ProjectionMatrix = glm::mat4(1.0f);
-		ProjectionMatrix = glm::perspective(glm::radians(fov), static_cast<float>(framebufferWidth) / framebufferHeight, nearPlane, farPlane);
-
-		glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "ProjectionMatrix"), 1, GL_FALSE, glm::value_ptr(ProjectionMatrix));
+		ProjectionMatrix = glm::mat4(1.0f); // Reinicializa a matriz de projeção
+		ProjectionMatrix = glm::perspective(glm::radians(fov), static_cast<float>(framebufferWidth) / framebufferHeight, nearPlane, farPlane); // Calcula a matriz de projeção
+		glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "ProjectionMatrix"), 1, GL_FALSE, glm::value_ptr(ProjectionMatrix)); // Define a matriz de projeção no shader
 
 		for (int i = 0; i < listaPlanetas.size(); i++)
 		{
-			listaPlanetas[i]->transform();
+			listaPlanetas[i]->transform(); // Aplica as transformações no planeta
+
 			GLuint texture;
 			for (Group* g : listaPlanetas[i]->mesh->groups) {
 				for (Material* m : listaPlanetas[i]->materials) {
 					texture = m->texture;
-					glUniform3f(glGetUniformLocation(shaderProgram, "Ka"), m->ka->r, m->ka->g, m->ka->b);
-					glUniform3f(glGetUniformLocation(shaderProgram, "Kd"), m->kd->r, m->kd->g, m->kd->b);
-					glUniform3f(glGetUniformLocation(shaderProgram, "Ks"), m->ks->r, m->ks->g, m->ks->b);
-					glUniform1f(glGetUniformLocation(shaderProgram, "Ns"), m->ns);
+					glUniform3f(glGetUniformLocation(shaderProgram, "Ka"), m->ka->r, m->ka->g, m->ka->b); // Define a reflectividade ambiente no shader
+					glUniform3f(glGetUniformLocation(shaderProgram, "Kd"), m->kd->r, m->kd->g, m->kd->b); // Define a reflectividade difusa no shader
+					glUniform3f(glGetUniformLocation(shaderProgram, "Ks"), m->ks->r, m->ks->g, m->ks->b); // Define a reflectividade especular no shader
+					glUniform1f(glGetUniformLocation(shaderProgram, "Ns"), m->ns); // Define a rugosidade no shader
 				}
 
-				glActiveTexture(GL_TEXTURE0);
-				glBindTexture(GL_TEXTURE_2D, texture);
-				glUniform1i(glGetUniformLocation(shaderProgram, "texture"), 0);
+				glActiveTexture(GL_TEXTURE0); // Ativa a textura
+				glBindTexture(GL_TEXTURE_2D, texture); // Vincula a textura ao objeto
+				glUniform1i(glGetUniformLocation(shaderProgram, "texture"), 0); // Define a unidade de textura no shader
 
-				glBindVertexArray(g->vao);
+				glBindVertexArray(g->vao); // Vincula o array de vértices
 
-				glUniform1i((glGetUniformLocation(shaderProgram, "planetSelecionado")), planetSelecionado == i + 1);
-				glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "ModelMatrix"), 1, GL_FALSE, glm::value_ptr(listaPlanetas[i]->ModelMatrix));
+				glUniform1i((glGetUniformLocation(shaderProgram, "planetSelecionado")), planetSelecionado == i + 1); // Define o planeta selecionado no shader
+				glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "ModelMatrix"), 1, GL_FALSE, glm::value_ptr(listaPlanetas[i]->ModelMatrix)); // Define a matriz de modelo no shader
 
-				glDrawArrays(GL_TRIANGLES, 0, g->faces.size() * 6);
+				glDrawArrays(GL_TRIANGLES, 0, g->faces.size() * 6); // Renderiza os triângulos
 			}
 		}
 
-		glfwPollEvents();
-		glfwSwapBuffers(window);
+		glfwPollEvents(); // Verifica os eventos da janela GLFW
+		glfwSwapBuffers(window); // Troca os buffers da janela GLFW
 	}
-	glfwTerminate();
+
+	glfwTerminate(); // Encerra a biblioteca GLFW
+
 	return 0;
 }
 
